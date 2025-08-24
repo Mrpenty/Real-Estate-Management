944cbd4449e678bfe213de703c181869e034435d
KhanhDo <dovietkhanh02082003@gmail.com>
2025-08-24 19:54:46 +0700

    deploy

--- RealEstateManagement.API/Extensions/DatabaseExtensions.cs
+++ RealEstateManagement.API/Extensions/DatabaseExtensions.cs
@@ -4,18 +4,28 @@ namespace RealEstateManagement.API.Extensions
 {
     public static class DatabaseExtensions
     {
-        public static IServiceCollection AddDatabaseServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddDatabaseServices(
+            this IServiceCollection services, IConfiguration configuration)
         {
-           services.AddDbContext<RentalDbContext>(options =>
-           {
-               options.UseSqlServer(configuration.GetConnectionString("MyCnn"),
-                   sql => sql.MigrationsAssembly("RealEstateManagement.Data")
-                   );
-           });
+            services.AddDbContext<RentalDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("MyCnn"),
+                    sql =>
+                    {
+                        // Migrations nằm trong project Data của bạn
+                        sql.MigrationsAssembly("RealEstateManagement.Data");
+
+                        // Retry khi DB mới khởi động/chưa sẵn sàng
+                        sql.EnableRetryOnFailure(
+                            maxRetryCount: 5,
+                            maxRetryDelay: TimeSpan.FromSeconds(5),
+                            errorNumbersToAdd: null);
+
+                        // (tuỳ chọn) tăng timeout lệnh
+                        sql.CommandTimeout(60);
+                    }));
+
             return services;
-
         }
-
-       
     }
 }
